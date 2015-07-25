2e38f951761e2be9b24bf9526e5d7a512e22fb21
Andrew Keeton <akeeton@users.noreply.github.com>
2015-07-25 17:10:02 -0400

    Gut Main.elm

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
 module Main where
 
-import ElmFire exposing (..)
-import ElmFire.Auth as Auth
+--import ElmFire exposing (..)
+--import ElmFire.Auth as Auth
 import Html exposing (..)
 import Html.Attributes exposing (id, type', width, height, src, style)
 import Html.Events exposing (onClick)
@@ -9,56 +9,25 @@ import Html.Events exposing (onClick)
 
 -- MODEL
 
-type Source = Youtube String
-videos =
-    List.map Youtube ["vR5HJp_xXRs", "RwpjyLUj0XU"]
-
-
-toHtml : Maybe Source -> Html
-toHtml maybeSource =
-  case maybeSource of
-    Nothing -> iframe [] []
-
-    Just (Youtube videoId) ->
-      let
-        srcUrl = "https://www.youtube.com/embed/" ++ videoId ++ "?autoplay=1&controls=0&disablekb=1&enablejsapi=1&fs=0&rel=0&iv_load_policy=3"
-      in
-        iframe
-          [ id "ytplayer"
-          , type' "text/html"
-          , width 720
-          , height 405
-          , src srcUrl
-          , style [("border", "0")]
-          ] []
-
-
 type alias Model =
-    List Source
+    Int
 
 
 -- UPDATE
-
-type Action = Start | Next | Stop
+type Action
+    = Nop
 
 
 update : Action -> Model -> Model
 update action model =
-  case action of
-    Start -> model
-    Next -> Maybe.withDefault [] <| List.tail model
-    Stop -> []
+    model
 
 
 -- VIEW
 
 view : Signal.Address Action -> Model -> Html
 view address model =
-  div []
-    [ button [ onClick address Stop ] [ text "Stop" ]
-    , button [ onClick address Next ] [ text "Next" ]
-    , toHtml <| List.head model
-    ]
+  div [] []
 
 countStyle : Attribute
 countStyle =
@@ -80,9 +49,9 @@ main =
 
 model : Signal Model
 model =
-  Signal.foldp update videos actions.signal
+  Signal.foldp update 0 actions.signal
 
 
 actions : Signal.Mailbox Action
 actions =
-  Signal.mailbox Start
+  Signal.mailbox Nop
