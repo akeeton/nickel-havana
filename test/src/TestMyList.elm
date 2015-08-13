71494d806159e865d4c9689b97a2d2938b33d261
Andrew Keeton <akeeton@users.noreply.github.com>
2015-08-13 14:01:24 -0400

    Add tests

--- test/src/TestMyList.elm
+++ test/src/TestMyList.elm
@@ -10,30 +10,31 @@ import ElmTest.Test exposing (..)
 import MyList exposing(..)
 
 
+zeroToFive = [0, 1, 2, 3, 4, 5]
+
+
+getAtTests = List.map defaultTest
+    [ Nothing `assertEqual` getAt zeroToFive -1
+    , Just 0 `assertEqual` getAt zeroToFive 0
+    , Just 3 `assertEqual` getAt zeroToFive 3
+    , Just 5 `assertEqual` getAt zeroToFive 5
+    , Nothing `assertEqual` getAt zeroToFive 6
+    , Nothing `assertEqual` getAt [] 0
+    , Nothing `assertEqual` getAt [] 5
+    ]
+
+removeAtTests = List.map defaultTest
+    [ zeroToFive `assertEqual` removeAt zeroToFive -1
+    , [1, 2, 3, 4, 5] `assertEqual` removeAt zeroToFive 0
+    , [0, 2, 3, 4, 5] `assertEqual` removeAt zeroToFive 1
+    , [0, 1, 2, 3, 4] `assertEqual` removeAt zeroToFive 5
+    , zeroToFive `assertEqual` removeAt zeroToFive 6
+    ]
+
+
 tests : Test
 tests =
-    let
-        zeroToFive = [0, 1, 2, 3, 4, 5]
-
-        getAtTests = List.map defaultTest
-            [ Nothing `assertEqual` getAt zeroToFive -1
-            , Just 0 `assertEqual` getAt zeroToFive 0
-            , Just 3 `assertEqual` getAt zeroToFive 3
-            , Just 5 `assertEqual` getAt zeroToFive 5
-            , Nothing `assertEqual` getAt zeroToFive 6
-            , Nothing `assertEqual` getAt [] 0
-            , Nothing `assertEqual` getAt [] 5
-            ]
-
-        removeAtTests = List.map defaultTest
-            [ zeroToFive `assertEqual` removeAt zeroToFive -1
-            , [1, 2, 3, 4, 5] `assertEqual` removeAt zeroToFive 0
-            , [0, 2, 3, 4, 5] `assertEqual` removeAt zeroToFive 1
-            , [0, 1, 2, 3, 4] `assertEqual` removeAt zeroToFive 5
-            , zeroToFive `assertEqual` removeAt zeroToFive 6
-            ]
-    in
-        suite "MyList Tests" <| getAtTests ++ removeAtTests
+    suite "MyList Tests" <| getAtTests ++ removeAtTests
 
 
 main : Html
