c19084f845d717e169b9dd15fee10f4deb3788cf
Andrew Keeton <akeeton@users.noreply.github.com>
2015-08-14 15:10:10 -0400

    Add template for creating a container of things

---
+++ etc/WidgetContainer.elm
@@ -0,0 +1,46 @@
+module WidgetContainer
+    ( WidgetContainer
+    , Action
+    , init
+    , update
+    , view
+    )
+    where
+
+import Html exposing (..)
+
+import Widget exposing (Widget)
+
+
+type alias WidgetContainer =
+    { widgets: List Widget
+    }
+
+
+type Action
+    = DoNothing
+    | WidgetAction (Widget.Action)
+
+
+init : List Widget -> WidgetContainer
+init widgets =
+    { widgets = widgets }
+
+
+update : Action -> WidgetContainer -> WidgetContainer
+update action container =
+    case action of
+        WidgetAction action ->
+            container
+
+        DoNothing ->
+            container
+
+
+view : Signal.Address Action -> WidgetContainer -> Html
+view address container =
+    let
+        forwardingAddress = Signal.forwardTo address WidgetAction
+        widgetsHtmls = List.map (Widget.view forwardingAddress) container.widgets
+    in
+        div [] widgetsHtmls
